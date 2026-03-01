43130deaaf67e8ea838cd190cdb7e1c943760158
ThiagoM8 <thiagomendesc998@gmail.com>
2026-03-01 15:20:49 -0300

    ADD POLYMORPHISM

--- app.py
+++ app.py
@@ -5,10 +5,14 @@ from modelos.cardapio.bebida import Bebida
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 prato_pao = Prato('Pão Francês', 2.0, 'O melhor da cidade')
+prato_pao.aplicar_desconto()
 bebida_suco = Bebida('Suco de Melância', 5.0, 'Grande')
+bebida_suco.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_pao)
 
 def main():
-    pass
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
